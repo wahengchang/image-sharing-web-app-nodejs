bb7e92f0bd00594537e86e575ccbccfdf8a0f794
wahengchang <wahengchang@gmail.com>
2021-10-14 12:05:49 +0800

    Added create react app

--- server/index.js
+++ server/index.js
@@ -5,8 +5,6 @@
     const DB = require('./lib/Db')
     await DB.init()
     app.use(require('./middleware/expressFunction'))
-
-    app.use(express.static('public'))
   
     app.use('/images', require('./routers/images'))
     app.use('/user', require('./routers/users'))
@@ -16,8 +14,9 @@
       const title = `title-${new Date().getTime()}`
       return res.send(`Hello World! : ${title}`)
     })
-  
-    const port = process.env.PORT || 3000
+    app.use(express.static('public'))
+
+    const port = process.env.PORT || 4000
   
     app.listen(port, function () {
       console.log(`Example app listening on port ${port}! \n http://localhost:${port}`)
