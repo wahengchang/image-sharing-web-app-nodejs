950de7657355d31875432aeadcc6c6cfc6ede4b6
wahengchang <wahengchang@gmail.com>
2021-10-18 14:11:00 +0800

    added e2e test

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -61,20 +61,20 @@ export default function LoginPage() {
       <div class='formContainer'>
         <div>
           Username<br />
-          <input type="text" {...username} autoComplete="username" />
+          <input type="text" {...username} autoComplete="username" name='username'/>
           {errorValidList.username && <><br /><small style={{ color: 'red' }}>{errorValidList.username}</small><br /></>}
         </div>
         <div style={{ marginTop: 10 }}>
           Password<br />
-          <input type="password" {...password} autoComplete="new-password" />
+          <input type="password" {...password} autoComplete="password" name='password'/>
           {errorValidList.password && <><br /><small style={{ color: 'red' }}>{errorValidList.password}</small><br /></>}
         </div>
         <div style={{ marginTop: 10 }}>
-          <input type="button" value={loading ? 'Loading...' : 'Login'} onClick={onClickLogin} disabled={loading} /><br />
+          <input id='loginSubmit' type="button" value={loading ? 'Loading...' : 'Login'} onClick={onClickLogin} disabled={loading} /><br />
         </div>
         {error && <><small style={{ color: 'red' }}>{error}</small><br /></>}<br />
       </div>
-      <Link to='/signup'> Signup </Link>
+      <Link id='sigupButton' to='/signup'> Signup </Link>
     </div>
   );
 }
